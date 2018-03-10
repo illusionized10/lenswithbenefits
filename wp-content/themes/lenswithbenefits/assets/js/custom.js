f882d12bbfdbcec34aa38a0a622ebe3eed0a764a
illusionized10 <colincohan10@gmail.com>
2018-03-09 16:01:54 -0800

    ADDITIONAL CHANGES

--- wp-content/themes/lenswithbenefits/assets/js/custom.js
+++ wp-content/themes/lenswithbenefits/assets/js/custom.js
@@ -3,7 +3,6 @@ $( document ).ready(function() {
     // Check to see if we are on the front page by checking the body class 
 	if ($("body").hasClass("page-template-tp-front-page")) {
 		// We are on the front page, start the owl carousel
-
 		$(".owl-carousel").owlCarousel({
 			video: true,
 			items: 1,
@@ -11,5 +10,14 @@ $( document ).ready(function() {
 			loop: true,
 		});
 	}
+
+	// Add border divs to active menu item after page has been loaded
+	var activeMenuItem = $(".site-header ul").find('li.current_page_item');
+	activeMenuItem.prepend(
+		'<span class="corner TL"></span>' +
+    	'<span class="corner TR"></span>' +
+    	'<span class="corner BL"></span>' +
+    	'<span class="corner BR"></span>'
+	);
 
 });
