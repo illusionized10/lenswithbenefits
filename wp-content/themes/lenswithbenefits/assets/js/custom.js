c53a5141c01876aaf60422b058c7ac57adfb48d9
Calin Cohan <colincohan10@gmail.com>
2018-04-03 15:47:46 -0700

    Additional changes.

--- wp-content/themes/lenswithbenefits/assets/js/custom.js
+++ wp-content/themes/lenswithbenefits/assets/js/custom.js
@@ -1,9 +1,9 @@
-$( document ).ready(function() {
+jQuery( document ).ready(function() {
     
     // Check to see if we are on the front page by checking the body class 
-	if ($("body").hasClass("page-template-tp-front-page")) {
+	if (jQuery("body").hasClass("page-template-tp-front-page")) {
 		// We are on the front page, start the owl carousel
-		$(".owl-carousel").owlCarousel({
+		jQuery(".owl-carousel").owlCarousel({
 		    loop:true,
 		    nav:true,
 		    items: 1,
@@ -11,12 +11,12 @@ $( document ).ready(function() {
 		});
 
 		// remove the text from the nav buttons, leaving images only
-		$(".owl-prev").empty();
-		$(".owl-next").empty();
+		jQuery(".owl-prev").empty();
+		jQuery(".owl-next").empty();
 	}
 
 	// Add border divs to active menu item after page has been loaded
-	var activeMenuItem = $(".site-header ul").find('li.current_page_item');
+	var activeMenuItem = jQuery(".site-header ul").find('li.current_page_item');
 	activeMenuItem.prepend(
 		'<span class="corner TL"></span>' +
 		'<span class="corner TLT"></span>' +
--- wp-content/themes/lenswithbenefits/tp-front-page.php
+++ wp-content/themes/lenswithbenefits/tp-front-page.php
@@ -21,34 +21,46 @@ get_header(); ?>
 
 <div class="introduction-slider">
 	<div class="owl-carousel owl-theme">
-		<?php foreach($slider_posts as $item) { ?>
 
-			<?php if(get_field('cc_slider_video_url', $item->ID)) { ?>
-				<div class="item video" style="background-image:url('<?php echo get_field("cc_video_background_image", $item->ID); ?>');">
-					<div class="content">
-						<a href="<?php echo get_field('cc_slider_video_url', $item->ID); ?>" class="mobx">
-							<i class="far fa-play-circle"></i>
-						</a>
-					</div>
-					<div class="slide-text">
-						<div class="title"><?php echo $item->post_title; ?></div>
-						<div class="subtitle"><?php echo get_field('cc_slide_subtitle', $item->ID); ?></div> 
-					</div>
-				</div>
-			<?php } else { ?>
-				<div class="item image" style="">
-					<div class="content">
-						<div class="title"><?php echo $item->post_title; ?></div>
-					</div>
-				</div>
-			<?php }  //endif ?> 
 
-		<?php } //end foreach?>
+			<?php foreach($slider_posts as $item) { ?>
+
+				<?php if(get_field('cc_slider_video_url', $item->ID)) { ?>
+					<div class="item video" style="background-image:url('<?php echo get_field("cc_video_background_image", $item->ID); ?>');">
+						<div class="content">
+							<a href="<?php echo get_field('cc_slider_video_url', $item->ID); ?>" class="mobx">
+								<i class="far fa-play-circle"></i>
+							</a>
+						</div>
+						<div class="slide-text">
+							<div class="title"><?php echo $item->post_title; ?></div>
+							<div class="subtitle"><?php echo get_field('cc_slide_subtitle', $item->ID); ?></div> 
+						</div>
+					</div>
+				<?php } else { ?>
+					<div class="item image" style="">
+						<div class="content">
+							<div class="title"><?php echo $item->post_title; ?></div>
+						</div>
+					</div>
+				<?php }  //endif ?> 
+
+			<?php } //end foreach?>
+
 	</div>
 </div>
 
 <div class="second-panel">
-	
+	<div class="wrapper">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="http://via.placeholder.com/660x805">
+			</div>
+			<div class="col-md-6">
+				<h2>Add an interesting title.</h2>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php get_footer();
