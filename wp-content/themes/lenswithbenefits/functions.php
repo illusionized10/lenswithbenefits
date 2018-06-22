af726ec75e79c631920a2fd5de4f84aaf6ddace0
illusionized10 <colincohan10@gmail.com>
2018-06-22 09:42:14 -0700

    Partial integration of services section

--- wp-content/themes/lenswithbenefits/functions.php
+++ wp-content/themes/lenswithbenefits/functions.php
@@ -239,4 +239,67 @@ function cc_create_slider() {
 }
 add_action( 'init', 'cc_create_slider', 0 );
 
+// Register Custom Post Type
+function cc_create_services() {
+
+	$labels = array(
+		'name'                  => 'Services',
+		'singular_name'         => 'Service',
+		'menu_name'             => 'Services',
+		'name_admin_bar'        => 'Services',
+		'archives'              => 'Service Archives',
+		'attributes'            => 'Service Attributes',
+		'parent_item_colon'     => 'Parent Services:',
+		'all_items'             => 'All Services',
+		'add_new_item'          => 'Add New Service',
+		'add_new'               => 'Add New Service',
+		'new_item'              => 'New Service',
+		'edit_item'             => 'Edit Service',
+		'update_item'           => 'Update Service',
+		'view_item'             => 'View Service',
+		'view_items'            => 'View Services',
+		'search_items'          => 'Search Service',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Service',
+		'uploaded_to_this_item' => 'Uploaded to this Service',
+		'items_list'            => 'Services list',
+		'items_list_navigation' => 'Services list navigation',
+		'filter_items_list'     => 'Filter Services list',
+	);
+	$rewrite = array(
+		'slug'                  => 'services',
+		'with_front'            => true,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
+	$args = array(
+		'label'                 => 'Service',
+		'description'           => 'List of services',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-images-alt2',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'rewrite'               => $rewrite,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'cc_custom_services', $args );
+
+}
+add_action( 'init', 'cc_create_services', 0 );
+
 show_admin_bar(false);
